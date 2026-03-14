6d8dd2f3c20f12a8fad0edc0094fe47a3b262a28
闫俊卓 <3182219477@qq.com>
2026-03-14 18:03:17 +0800

    测试是否正常

--- app/src/main/java/com/YanandWang/ourandroidproject/MainActivity.kt
+++ app/src/main/java/com/YanandWang/ourandroidproject/MainActivity.kt
@@ -6,6 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+/**
+ * 2026.3.14
+ * 项目创建
+ * 作者：闫俊卓 王希文
+ * 希望一切顺利，做出一个好玩的软件
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
