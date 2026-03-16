b13950985f856d7928215cddc4be7e1f990eca0b
闫俊卓 <3182219477@qq.com>
2026-03-16 16:37:52 +0800

    feat: 完成初始页面制作

---
+++ app/src/main/java/com/YanandWang/ourandroidproject/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.YanandWang.ourandroidproject
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.OnBackPressedCallback // 必须导入
+
+class SplashActivity : AppCompatActivity() {
+    private val delayTime: Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash) 
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, delayTime)
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, callback)
+    }
+}
